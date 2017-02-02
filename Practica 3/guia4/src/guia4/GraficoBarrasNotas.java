73589568f1be98cceb68829df6ce17d083937460
Cristian Sanchez <sancas19@gmail.com>
2017-02-02 16:57:58 -0600

    Agregada clase guia 4
    Se agregaron los ejemplos de la guia4

---
+++ Practica 3/guia4/src/guia4/GraficoBarrasNotas.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia4;
+
+/**
+ *
+ * @author sanch
+ */
+public class GraficoBarrasNotas {
+    public static void main(String[] args)
+    {
+        int array[] = { 0, 0, 0, 0, 0, 0, 3, 12, 6, 2, 1 };
+        System.out.println("Distribucion de Notas");
+        for (int contador = 0; contador < array.length; contador++)
+        {
+            if (contador == 10)
+                System.out.printf("%5d", 100);
+            else
+                System.out.printf("%02d-%02d: ", contador * 10, contador * 10 + 9);
+            for (int asteriscos = 0; asteriscos < array[contador]; asteriscos++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
---
+++ Practica 3/guia4/src/guia4/Matriz02.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia4;
+
+/**
+ *
+ * @author sanch
+ */
+public class Matriz02 {
+    public static void main(String[] args)
+    {
+        int dias_del_mes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.println("Abril tiene " + dias_del_mes[3] + " días");
+    }
+}
